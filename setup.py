415df410f2663d51152d4a2de66f46ce6dd44356
Rasmus Pedersen <rap@serenergy.com>
2018-04-26 15:51:28 +0200

    Fixed syntax error in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 setup(
     name="onewiretoinflux",
     version="1.0.0",
-    scripts=['logtemperatures.py']
+    scripts=['logtemperatures.py'],
     install_requires=['requests', 'influxdb']
 )
