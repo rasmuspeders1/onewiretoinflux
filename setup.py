5bf38dff795facf6b2508246959839fe8b3b917b
Rasmus Pedersen <rap@serenergy.com>
2018-04-26 15:45:34 +0200

    changing to use scripts in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@ from setuptools import setup
 setup(
     name="onewiretoinflux",
     version="1.0.0",
-    packages=['onewiretoinflux'],
-    entry_points={
-        'console_scripts': ['logtemperatures=onewiretoinflux.logtemperatures']
-    },
+    scripts=['logtemperatures.py']
     install_requires=['requests', 'influxdb']
 )
